a2d9ea8ad228d2b3b63f482cdaff469d96a5de77
billalhaddad <billal.haddad.stud@ispascalcomandini.it>
2021-11-12 08:50:31 +0100

    Aggiunta metodi

--- bibliotecaScolastica/Biblioteca.cs
+++ bibliotecaScolastica/Biblioteca.cs
@@ -8,5 +8,46 @@ namespace bibliotecaScolastica
 {
     class Biblioteca
     {
+        public string _nome { get; set; }
+        public string _indirizzio { get; set; }
+        public int Time { get; set; }
+        public int TimeChiusura { get; set; }
+        public List<Libro> _libri { get; set; }
+        public Biblioteca(string nome,string indirizzo,int time,int tempoChius,List<Libro> libri)
+        {
+            _nome = nome;
+            _indirizzio = indirizzo;
+            Time = time;
+            TimeChiusura = tempoChius;
+            _libri = libri;
+        }
+        public void AggiuntaLibro(Libro l)
+        {
+            _libri.Add(l);
+        }
+        public Libro ricercaLibroConTitolo(string Titolo)
+        {
+            Libro libro=new Libro("",Titolo,new DateTime(2000,1,1)," ",100);
+            foreach (Libro l in _libri)
+            {
+                if (l._titolo == Titolo)
+                {
+                    libro = l;
+                }
+            }
+            return libro;
+        }
+        public List<Libro> ricercaLibriAutore(string autore)
+        {
+            List<Libro> libriAutore = new List<Libro>();
+            foreach (Libro l in _libri)
+                if (l._autore == autore)
+                    libriAutore.Add(l);
+            return libriAutore;
+        }
+        public int numeroLibri()
+        {
+            return _libri.Count;
+        }
     }
 }
