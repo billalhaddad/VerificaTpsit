9ee9775349d8146eac6af93a0a8c070b13848050
billalhaddad <billal.haddad.stud@ispascalcomandini.it>
2021-11-12 09:30:25 +0100

    aggiunta del xaml cs

--- bibliotecaScolastica/MainWindow.xaml.cs
+++ bibliotecaScolastica/MainWindow.xaml.cs
@@ -20,9 +20,44 @@ namespace bibliotecaScolastica
     /// </summary>
     public partial class MainWindow : Window
     {
+        Biblioteca b;
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            b = new Biblioteca(nome.Text, indirizzo.Text, int.Parse(orarioApertura.Text), int.Parse(orariochiusura.Text));
+        }
+
+        private void trovaNLibri_Click(object sender, RoutedEventArgs e)
+        {
+            numLibri.Text = b.numeroLibri().ToString();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            libriAutore.ItemsSource = null;
+            libriAutore.ItemsSource = b.ricercaLibriAutore(aturore.Text);
+        }
+
+        private void cerca2_Click(object sender, RoutedEventArgs e)
+        {
+            risultato2.Text = b.ricercaLibroConTitolo(titolo.Text).ToString();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            Libro l = libri.SelectedItem as Libro;
+            tempolettura.Text = l.readingTine();
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            b.AggiuntaLibro(new Libro(aut.Text, tit.Text, new DateTime(int.Parse(anno.Text), 1, 1), edit.Text, int.Parse(nPg.Text)));
+            libri.ItemsSource = null;
+            libri.ItemsSource = b._libri;
         }
     }
 }
