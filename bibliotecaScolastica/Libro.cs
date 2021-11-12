4e76c5646af3042bc2fd19080d94b7f639bc0859
billalhaddad <billal.haddad.stud@ispascalcomandini.it>
2021-11-12 08:24:10 +0100

    aggiunta proprieta

---
+++ bibliotecaScolastica/Libro.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bibliotecaScolastica
+{
+    class Libro
+    {
+        public string _autore { get; set; }
+        public string _titolo { get; set; }
+        public DateTime _annoPubblicazione { get; set; }
+        public string _editore { get; set; }
+        public int _nPagine{get;set;}
+        public Libro(string autore,string titolo,DateTime annoPUb,string editore,int nPag)
+        {
+            _autore = autore;
+            _titolo = titolo;
+            _annoPubblicazione = annoPUb;
+            _editore = editore;
+            _nPagine = nPag;
+        }
+
+    }
+}
